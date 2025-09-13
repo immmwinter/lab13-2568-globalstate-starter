a86e3955bb0f5089f39b0cfedd63bf7a3b126b08
frostbite <get.bhumiphat@gmail.com>
2025-09-13 18:10:28 +0700

    local

--- src/store/TaskItemStore.tsx
+++ src/store/TaskItemStore.tsx
@@ -1,39 +1,55 @@
 import { create } from "zustand";
+import { persist, createJSONStorage  } from "zustand/middleware";
 import { v4 as uuidv4 } from "uuid";
 import { type TaskItemProps } from "../libs/Task";
 
-export const useTaskStore = create<TaskItemProps>((set) => ({
-  tasks: [], //เริ่มต้น
-  setTasks: (tasks) => set({ tasks }),
-  addTask: (title, description, dueDate, assignees) =>
-    set((state) => ({
-      tasks: [
-        ...state.tasks,
-        {
-          id: uuidv4(),
-          title,
-          description,
-          dueDate,
-          assignees,
-          isDone: false,
-          doneAt: null,
-        },
-      ],
-    })),
-  toggleTask: (id) =>
-    set((state) => ({
-      tasks: state.tasks.map((task) =>
-        task.id === id
-          ? {
-              ...task,
-              isDone: !task.isDone,
-              doneAt: task.isDone ? null : new Date().toLocaleDateString(),
-            }
-          : task
-      ),
-    })),
-  removeTask: (id) =>
-    set((state) => ({
-      tasks: state.tasks.filter((task) => task.id !== id),
-    })),
-}));
+export const useTaskStore = create<TaskItemProps>()(
+  persist(
+    (set) => ({
+      tasks: [],
+
+      setTasks: (tasks) => set({ tasks }),
+
+      addTask: (title, description, dueDate, assignees) =>
+        set((state) => ({
+          tasks: [
+            ...state.tasks,
+            {
+              id: uuidv4(),
+              title,
+              description,
+              dueDate,
+              assignees,
+              isDone: false,
+              doneAt: null,
+            },
+          ],
+        })),
+
+      toggleTask: (id) =>
+        set((state) => ({
+          tasks: state.tasks.map((task) =>
+            task.id === id
+              ? {
+                  ...task,
+                  isDone: !task.isDone,
+                  doneAt: task.isDone
+                    ? null
+                    : new Date().toLocaleDateString(),
+                }
+              : task
+          ),
+        })),
+
+      removeTask: (id) =>
+        set((state) => ({
+          tasks: state.tasks.filter((task) => task.id !== id),
+        })),
+    }),
+    {
+      name: "my-task", 
+      storage: createJSONStorage(() => localStorage), 
+      partialize: (state) => ({ tasks: state.tasks }),
+    }
+  )
+);
